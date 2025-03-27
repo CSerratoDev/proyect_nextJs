6662350776b64aee97bb103f49a4f4409ebb029d
CSerratoDev <cserrato.dev@gmail.com>
2025-03-26 21:50:25 -0600

    Added Server Side Components and Fetching with SSC

--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const CountPage = async () => {
-    const countLocations = await axios.get("http://127.0.0.1:4000/locations")
-    return "Total de Localizaciones: " + countLocations?.data?.length;
+    const counLocations = await axios.get('http://127.0.0.1:4000/locations');
+    return "Hay: " + counLocations?.data?.length + " Locations"; 
 }
 
 export default CountPage;
