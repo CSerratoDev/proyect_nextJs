bf19ef39bca420efd4d27174aa0c2bfd66f91726
CSerratoDev <cserrato.dev@gmail.com>
2025-04-01 15:49:33 -0600

    Added Disable & Router

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,18 +2,18 @@
 import Image from "next/image";
 
 export default function AuthLayout({
-    children, 
-}:  Readonly<{ 
-    children: React.ReactNode; 
+    children,
+}: Readonly<{
+    children: React.ReactNode;
 }>) {
     return (
         <div className="bg-orange-200 w-screen h-screen overflow-hidden grid">
             <div className="place-content-center place-self-center place-items-center text-center">
                 <div className="flex flex-col items-center bottom-10 relative">
-                    <Image 
+                    <Image
                         src="/shop.svg"
-                        alt="logo" 
-                        width={120} 
+                        alt="logo"
+                        width={120}
                         height={60}
                     />
                 </div>
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,27 +1,35 @@
 "use client";
-import { Button, Input } from "@heroui/react";
+import { Button, Input, Spinner } from "@heroui/react";
 import axios from "axios";
 import Link from "next/link";
 import { API_URL } from "../../../constants";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
-
+    const [submit, setSubmit] = useState(false);
+    const router = useRouter();
     const handleSubmit = async (e: React.FormEvent) => {
+        setSubmit(true);
         e.preventDefault();
-        
         const formData = new FormData(e.target);
         let authData: any = {};
         authData.userEmail = formData.get("userEmail") as string;
         authData.userPassword = formData.get("userPassword") as string;
-        const {data} = await axios.post(`${API_URL}/auth/login`, {
-            ... authData
-        }, {
-            withCredentials: true,
-        });
-        console.log(data);
+        try {
+            const response = await axios.post(`${API_URL}/auth/login`, {
+                ...authData
+            }, {
+                withCredentials: true,
+            });
+            if (response.status === 201) router.push("/dashboard");
+            setSubmit(false)
+        } catch (error) {
+            setSubmit(false);
+        }
         return;
     }
-        
+
     return (
         <form className="bg-red-400 px-10 py-5 rounded-md" onSubmit={handleSubmit}>
             <p className="text-2xl my-4">
@@ -32,8 +40,8 @@ export default function Login() {
                 <Input label="Password" name="userPassword" type="password" isRequired={true} size="sm" />
             </div>
             <div className="flex flex-col items-center gap-2 py-3">
-                <Button color="primary" type="submit">
-                    Login    
+                <Button color="primary" type="submit" disabled={submit}>
+                    {submit ? "Enviando..." : "Login"}
                 </Button>
                 <p>
                     Don't have an account? <Link href="/signup" className="underline">Sign Up</Link>
